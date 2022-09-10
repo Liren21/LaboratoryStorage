c07c95dc261eab3cb4d159a1385d73db4a00aa06
Liren21 <40273016+Liren21@users.noreply.github.com>
2022-09-11 00:38:14 +0600

    Update laboratoryWorkOne.java

--- laboratoryWorkOne.java
+++ laboratoryWorkOne.java
@@ -1,98 +1,103 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class laboratoryWorkOne {
+public class LaboratoryWorkTwo {
 
+    public static final String sANSI_RED = "\u001B[31m";
+    public static final String sANSI_GREEN = "\u001B[32m";
+    public static final String sANSI_BLUE = "\u001B[34m";
+    public static final String sANSI_PURPLE = "\u001B[35m";
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
-
-    public static void menu() {
+    public static void vMenu() {
         System.out.print(
-                ANSI_GREEN + "\n--------------------------------------------------------------------\n\n" +
+                sANSI_GREEN + "\n--------------------------------------------------------------------\n\n" +
                         "Приветствую тебя пользователь, программа обладает следующим функционалом:\n" +
                         "[0] Выход\n" +
                         "[1] Вывод в консоль 'Hello World!'\n" +
                         "[2] Калькулятор по формуле   \n " +
                         "\n--------------------------------------------------------------------\n" +
                         "\n" +
-                        "Выберите пункт:" + ANSI_GREEN
+                        "Выберите пункт:" + sANSI_GREEN
         );
         Scanner in = new Scanner(System.in);
         String valueInput = in.nextLine();
 
         switch (valueInput) {
-            case ("0") -> exitProgram();
+            case ("0") -> vExitProgram();
             case ("1") -> {
-                helloWorld();
-                menu();
+                vHelloWorld();
+                vMenu();
             }
             case ("2") -> {
-                calc();
-                menu();
+                vCalc();
+                vMenu();
             }
             default -> {
-                System.out.print(ANSI_RED + "Введите число из представленного меню" + ANSI_RED);
-                menu();
+                System.out.print(sANSI_RED + "Введите число из представленного меню" + sANSI_RED);
+                vMenu();
             }
         }
     }
 
     public static void main(String[] args) {
-        menu();
+        vMenu();
     }
 
-    public static void helloWorld() {
-        System.out.print(ANSI_BLUE + "Вы выбрали: Вывод в консоль \n'Hello World!\n" + ANSI_BLUE);
-        menu();
+    public static void vHelloWorld() {
+        System.out.print(sANSI_BLUE + "Вы выбрали: Вывод в консоль \n'Hello World!\n" + sANSI_BLUE);
     }
 
-    public static void exitProgram() {
+    public static void vExitProgram() {
         System.out.print("Вы выбрали: Выход из программы.\nДо свидания, надеемся, что вы еще вернетесь.\n");
         System.exit(0);
     }
 
-    public static void calc() {
+    public static void vCalc() {
         //Z/X + 7*sqrt(Y)
         Scanner in = new Scanner(System.in);
         System.out.print(
-                ANSI_BLUE + "Формула: Z/X + 7*sqrt(Y)\n" + ANSI_BLUE
+                sANSI_BLUE + "Формула: Z/X + 7*sqrt(Y)\n" + sANSI_BLUE
         );
         try {
             System.out.print(
-                    ANSI_BLUE + "Введите число X: " + ANSI_BLUE
+                    sANSI_BLUE + "Введите число X: " + sANSI_BLUE
             );
             int inputValX = in.nextInt();
+            if (inputValX == 0) {
+                System.out.print(
+                        sANSI_RED + "Вводимое число не должно быть меньше или равное 0. \nПовторите ввод заново\n" + sANSI_RED
+                );
+                vCalc();
+            }
             System.out.print(
-                    ANSI_BLUE + "Введите число Y: " + ANSI_BLUE
+                    sANSI_BLUE + "Введите число Y: " + sANSI_BLUE
             );
             int inputValY = in.nextInt();
+            if (inputValY < 0) {
+                System.out.print(
+                        sANSI_RED + "Вводимое число не должно быть меньше или равное 0. \nПовторите ввод заново\n" + sANSI_RED
+                );
+                vCalc();
+            }
             System.out.print(
-                    ANSI_BLUE + "Введите число Z: " + ANSI_BLUE
+                    sANSI_BLUE + "Введите число Z: " + sANSI_BLUE
+
             );
             int inputValZ = in.nextInt();
             double sum = (inputValZ / inputValX) + (7 * Math.sqrt(inputValY));
 
             String formatNumber = new DecimalFormat("#0.000").format(sum);
             if (!Double.isNaN(sum)) {
-                System.out.printf(ANSI_PURPLE + "Ваш ответ: " + formatNumber + ANSI_PURPLE);
+                System.out.printf(sANSI_PURPLE + "Ваш ответ: " + formatNumber + sANSI_PURPLE);
             } else {
-                System.out.printf(ANSI_PURPLE + "Ваш ответ: Не число поэтому перевроверьте вводиммые числа, особенно то, что в корн\n" + ANSI_PURPLE);
-                calc();
+                System.out.printf(sANSI_PURPLE + "Ваш ответ: Не число поэтому провероверьте вводиммые числа, особенно то, что в корне\n" + sANSI_PURPLE);
+                vCalc();
             }
         } catch (Exception e) {
 
-            System.out.print(ANSI_RED + "Ошибка: введен недопустимый символ.\n" +
-                    "Пожалуйста, проверьте корректность вводимых данных\n" + ANSI_RED);
-            calc();
+            System.out.print(sANSI_RED + "Ошибка: введен недопустимый символ.\n" +
+                    "Пожалуйста, проверьте корректность вводимых данных\n" + sANSI_RED);
+            vCalc();
         }
 
     }
