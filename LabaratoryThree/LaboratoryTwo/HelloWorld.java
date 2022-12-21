66fa2e38ccaeac0e2e620cfc6ff51ff698eb453e
Liren21 <40273016+Liren21@users.noreply.github.com>
2022-12-21 20:53:58 +0600

    Delete LabaratoryThree/LaboratoryTwo directory

--- LabaratoryThree/LaboratoryTwo/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package LabaratoryThree.LaboratoryTwo;
-
-public class HelloWorld {
-
-    public static void vWithdrawalHelloWord() {
-        System.out.print(TextPaint.sANSI_BLUE + "Вы выбрали: Вывод в консоль \n'Hello World!\n" + TextPaint.sANSI_BLUE);
-    }
-}
--- LabaratoryThree/LaboratoryTwo/LaboratoryWorkTree.java
+++
@@ -1,9 +0,0 @@
-package LabaratoryThree.LaboratoryTwo;
-
-public class LaboratoryWorkTree {
-
-    public static void main(String[] args) throws Exception {
-        Menu.vMenu();
-    }
-
-}
