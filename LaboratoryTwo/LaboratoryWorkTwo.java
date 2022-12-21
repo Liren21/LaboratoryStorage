b01d8ff9b909ec49f35558d9594d6eaa5a9d4118
Liren21 <40273016+Liren21@users.noreply.github.com>
2022-12-21 20:54:06 +0600

    Delete LaboratoryTwo directory

--- LaboratoryTwo/LaboratoryWorkTwo.java
+++
@@ -1,9 +0,0 @@
-package LaboratoryTwo;
-
-public class LaboratoryWorkTwo {
-
-    public static void main(String[] args) {
-        Menu.vMenu();
-    }
-
-}
--- LaboratoryTwo/TextPaint.java
+++
@@ -1,8 +0,0 @@
-package LaboratoryTwo;
-
-public class TextPaint {
-    public static final String sANSI_RED = "\u001B[31m";
-    public static final String sANSI_GREEN = "\u001B[32m";
-    public static final String sANSI_BLUE = "\u001B[34m";
-    public static final String sANSI_PURPLE = "\u001B[35m";
-}
