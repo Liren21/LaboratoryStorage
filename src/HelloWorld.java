7539dcbb753d4200f8eee0eeea1a4527096b1233
Liren21 <40273016+Liren21@users.noreply.github.com>
2022-12-25 22:25:41 +0600

    Delete HelloWorld.java

--- src/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-
-    public static void vWithdrawalHelloWord() {
-        System.out.print(TextPaint.sANSI_BLUE + "Вы выбрали: Вывод в консоль \n'Hello World!\n" + TextPaint.sANSI_BLUE);
-    }
-}
